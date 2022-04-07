ff40ecb8b6d0bde20f71f7e154a888fc5b9e2757
Rafael Prado <ra.pcamargo@hotmail.com>
2022-04-07 10:58:53 -0300

    Adicionar Seção Perguntas Frequentes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 // When the user scrolls the page, execute myFunction
 
-window.onscroll = function() {myFunction()};
+window.onscroll = function () { myFunction() };
 
 
 // Get the header
@@ -27,4 +27,32 @@ function myFunction() {
 
   }
 
+}
+
+
+
+
+
+
+
+
+
+
+var acc = document.getElementsByClassName("perguntas-item");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
 }
