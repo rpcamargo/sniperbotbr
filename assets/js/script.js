0baccec8473367f1c7eca2b0fb059a7b2fec41d4
Rafael Prado <ra.pcamargo@hotmail.com>
2022-04-11 11:07:55 -0300

    Alteração nas perguntas frequentes, icone de aberto e fechado

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,10 @@
-// When the user scrolls the page, execute myFunction
+
 window.onscroll = function () { myFunction() };
-// Get the header
+
 var header = document.getElementById("header");
-// Get the offset position of the navbar
+
 var sticky = header.offsetTop;
-// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+
 
 
 
@@ -21,18 +21,19 @@ function myFunction() {
 var acc = document.getElementsByClassName("perguntas-item");
 var i;
 
+
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function () {
-    /* Toggle between adding and removing the "active" class,
-    to highlight the button that controls the panel */
-    this.classList.toggle("active");
 
-    /* Toggle between hiding and showing the active panel */
+
+    this.classList.toggle("active");
     var panel = this.nextElementSibling;
     if (panel.style.display === "block") {
       panel.style.display = "none";
+
     } else {
       panel.style.display = "block";
+
     }
   });
 }
